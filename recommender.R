2890fd5fa7ee8ee4b13557032e1cb18c83fd9fc2
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-01-29 20:16:51 +0100

    UBCF nn parameter benchmark

--- recommender.R
+++ recommender.R
@@ -97,9 +97,44 @@ similarityMethods.benchmark.saveData <- function(resultsMatrix, fileName, captio
 	close(ibcfSMTableFile)
 }
 
-results <- similarityMethods.benchmark(scheme)
+# z tego można narysować wykresy:
+#	- jak się zmienia błąd (MAE, MSE, RMSE) w zależności od nn
+# 	- jak się zmienia czas w zależności od nn
+#	- jak wyglądają krzywe ROC dla ewaluacji algorytmów z różnym nn
+ubcf.nn.benchmark <- function(scheme) {
+	ubcfCommonParameters = list(method="Cosine", normalize='center', minRating=4)
+	trainingData <- getData(scheme, "train")
+	knownData <- getData(scheme, "known")	
+	unknownData <- getData(scheme, "unknown")	
+	# od nn=5 ze skokiem 20, aż do całkowitej liczby użytkowników
+	nns <- seq(5, dim(trainingData)[1], 20)
+	algorithms <- list()
+	errorsAndTime <- c()
+	# TODO odkomentować tą pętle
+	# for(nn in nns) {
+	for(nn in 5:10) {
+		print(nn)
+		algorithmParameters <- c(ubcfCommonParameters, nn=nn)
+		algorithms[[paste("UBCF nn=", nn)]] = list(name="UBCF", param=algorithmParameters)
+		ubcf <- 0
+		# TODO ten czas wyświetla się jako user_time system_time elapsed_time a to nam nie potrzebne
+		#  jak zrobić z tego pojedynczą wartość np. w milisekundach??
+		timeToBuildRecommender <- system.time({
+			ubcf <- Recommender(trainingData, method="UBCF", parameter=algorithmParameters)
+		})
+		ubcf.predictions <- predict(ubcf, knownData, type="ratings")
+		errors <- calcPredictionError(ubcf.predictions, unknownData)
+		errorsAndTime <- rbind(c(nn, timeToBuildRecommender, errors), errorsAndTime)
+	}
+	evaluationResults <- evaluate(scheme,algorithms,n=c(1,3,5,10,20))
+	list(errorsAndTime = errorsAndTime, evaluationResults = evaluationResults)
+}
 
-similarityMethods.benchmark.saveData(results$ibcf.errors, "ibcfSimilarityTableErrors.tex", c('Porównanie metod podobieństwa dla IBCF'))
-similarityMethods.benchmark.saveData(results$ubcf.errors, "ubcfSimilarityTableErrors.tex", c('Porównanie metod podobieństwa dla UBCF'))
+#results <- similarityMethods.benchmark(scheme)
 
+#similarityMethods.benchmark.saveData(results$ibcf.errors, "ibcfSimilarityTableErrors.tex", c('Porównanie metod podobieństwa dla IBCF'))
+#similarityMethods.benchmark.saveData(results$ubcf.errors, "ubcfSimilarityTableErrors.tex", c('Porównanie metod podobieństwa dla UBCF'))
+
+
+results <- ubcf.nn.benchmark(scheme)
 
