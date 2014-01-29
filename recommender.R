68652d61cf2c0ad58012a0faf06fa9b8bb21c7e9
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-01-29 17:07:12 +0100

    Use MovieLense dataset. Add multiple variants of UBCF and IBCF

--- recommender.R
+++ recommender.R
@@ -4,23 +4,38 @@ library('recommenderlab')
 library('ROCR')
 
 #read all data from file and convert it into recommenderlab structure
-allData = createUsersMatrix(read.table(file.path("data", "u.data")))
+allData = MovieLense 
+# allData = createUsersMatrix(read.table(file.path("data", "u.data")))
 
 #read trainig and test data (80% - 20%)
-trainData = createUsersMatrix(read.table(file.path("data","u1.base")))
-testData = createUsersMatrix(read.table(file.path("data","u1.test")))
+#trainData = createUsersMatrix(read.table(file.path("data","u1.base")))
+#testData = createUsersMatrix(read.table(file.path("data","u1.test")))
 
 #recommendations
-recommender =  Recommender(trainData, method = "UBCF")   #create recommender, also UBCF, IBCF, RANDOM, POPULAR
-recommender2 =  Recommender(trainData, method = "IBCF")
+#recommender =  Recommender(trainData, method = "UBCF")   #create recommender, also UBCF, IBCF, RANDOM, POPULAR
+#recommender2 =  Recommender(trainData, method = "IBCF")
 #recoms = predict(recommender,type="topNList", testData[1:10], n=3)  #predict best 3 movies for first 10 test users
-recoms = predict(recommender,type="ratings", testData[1:10])  #predict all movie ratings for first 10 test users
-recomsMatrix = as(recoms,"matrix")[,1:10] #clip rating matrix to first 10 movies
+#recoms = predict(recommender,type="ratings", testData[1:10])  #predict all movie ratings for first 10 test users
+#recomsMatrix = as(recoms,"matrix")[,1:10] #clip rating matrix to first 10 movies
 
-scheme = evaluationScheme(allData, method="split",train=0.8)
+scheme = evaluationScheme(allData[1:100], method="split",train=0.8, goodRating = 4)
 algorithms <- list(
-   "random items" = list(name="RANDOM", param=NULL),   "popular items" = list(name="POPULAR", param=NULL),
-   "user-based CF" = list(name="UBCF", param=list(method="Cosine", nn=50, minRating=5))
+   "popular items" = list(name="POPULAR", param=NULL),
+   "user-based CF Cosine center nn50" = list(name="UBCF", param=list(method="Cosine", normalize='center', nn=50, minRating=5)),
+   "user-based CF Pearson center nn50" = list(name="UBCF", param=list(method="pearson", normalize='center', nn=50, minRating=5)), 
+   "user-based CF jaccard center nn50" = list(name="UBCF", param=list(method="jaccard", normalize='center', nn=50, minRating=5)),   
+   "user-based CF jaccard center nn50" = list(name="UBCF", param=list(method="jaccard", normalize='center', nn=50, minRating=5)),   
+   "user-based CF Pearson z-index nn50" = list(name="UBCF", param=list(method="pearson", normalize='Z-score', nn=50, minRating=5)),
+   "item-based CF Cosine center" = list(name="IBCF", param=(list(k = 30, method="Cosine", normalize = "center",  alpha = 0.5))),
+   "item-based CF pearson center" = list(name="IBCF", param=(list(k = 5, method="pearson", normalize = "center",  alpha = 0.5))),   	
+	"item-based CF karypis center" = list(name="IBCF", param=(list(k = 5, method="karypis", normalize = "center",  alpha = 0.5))),
+	"item-based CF jaccard center" = list(name="IBCF", param=(list(k = 5, method="jaccard", normalize = "center",  alpha = 0.5))),
+	"item-based CF conditional center" = list(name="IBCF", param=(list(k = 5, method="conditional", normalize = "center",  alpha = 0.5))),
+	"item-based CF Cosine Z-score" = list(name="IBCF", param=(list(k = 6, method="Cosine", normalize = "Z-score",  alpha = 0.5))),
+    "item-based CF pearson Z-score" = list(name="IBCF", param=(list(k = 7, method="pearson", normalize = "Z-score",  alpha = 0.5))),
+   	"item-based CF jaccard Z-score" = list(name="IBCF", param=(list(k = 5, method="jaccard", normalize = "Z-score",  alpha = 0.5))),
+	"item-based CF karypis Z-score" = list(name="IBCF", param=(list(k = 4, method="karypis", normalize = "Z-score",  alpha = 0.5))),
+	"item-based CF conditional Z-score" = list(name="IBCF", param=(list(k = 5, method="conditional", normalize = "Z-score",  alpha = 0.5)))
    )
 evalResults = evaluate(scheme,algorithms,n=c(1,3,5,10,20))
 
