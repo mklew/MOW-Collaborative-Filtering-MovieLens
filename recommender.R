ee5fd251331bfe0441292525ffc628ff1a6f13d8
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-01-29 17:41:17 +0100

    Remove IBCF with karypis because it does not work

--- recommender.R
+++ recommender.R
@@ -18,23 +18,21 @@ allData = MovieLense
 #recoms = predict(recommender,type="ratings", testData[1:10])  #predict all movie ratings for first 10 test users
 #recomsMatrix = as(recoms,"matrix")[,1:10] #clip rating matrix to first 10 movies
 
-scheme = evaluationScheme(allData[1:100], method="split",train=0.8, goodRating = 4)
+scheme = evaluationScheme(allData[1:300], method="split",train=0.8, goodRating = 4)
 algorithms <- list(
    "popular items" = list(name="POPULAR", param=NULL),
-   "user-based CF Cosine center nn50" = list(name="UBCF", param=list(method="Cosine", normalize='center', nn=50, minRating=5)),
-   "user-based CF Pearson center nn50" = list(name="UBCF", param=list(method="pearson", normalize='center', nn=50, minRating=5)), 
-   "user-based CF jaccard center nn50" = list(name="UBCF", param=list(method="jaccard", normalize='center', nn=50, minRating=5)),   
-   "user-based CF jaccard center nn50" = list(name="UBCF", param=list(method="jaccard", normalize='center', nn=50, minRating=5)),   
-   "user-based CF Pearson z-index nn50" = list(name="UBCF", param=list(method="pearson", normalize='Z-score', nn=50, minRating=5)),
+   "user-based CF Cosine center nn50" = list(name="UBCF", param=list(method="Cosine", normalize='center', nn=20, minRating=4)),
+   "user-based CF Pearson center nn50" = list(name="UBCF", param=list(method="pearson", normalize='center', nn=20, minRating=4)), 
+   "user-based CF jaccard center nn50" = list(name="UBCF", param=list(method="jaccard", normalize='center', nn=20, minRating=4)),   
+   "user-based CF jaccard center nn50" = list(name="UBCF", param=list(method="jaccard", normalize='center', nn=20, minRating=4)),   
+   "user-based CF Pearson z-index nn50" = list(name="UBCF", param=list(method="pearson", normalize='Z-score', nn=20, minRating=4))
    "item-based CF Cosine center" = list(name="IBCF", param=(list(k = 30, method="Cosine", normalize = "center",  alpha = 0.5))),
-   "item-based CF pearson center" = list(name="IBCF", param=(list(k = 5, method="pearson", normalize = "center",  alpha = 0.5))),   	
-	"item-based CF karypis center" = list(name="IBCF", param=(list(k = 5, method="karypis", normalize = "center",  alpha = 0.5))),
+   "item-based CF pearson center" = list(name="IBCF", param=(list(k = 5, method="pearson", normalize = "center",  alpha = 0.5))),   		
 	"item-based CF jaccard center" = list(name="IBCF", param=(list(k = 5, method="jaccard", normalize = "center",  alpha = 0.5))),
 	"item-based CF conditional center" = list(name="IBCF", param=(list(k = 5, method="conditional", normalize = "center",  alpha = 0.5))),
 	"item-based CF Cosine Z-score" = list(name="IBCF", param=(list(k = 6, method="Cosine", normalize = "Z-score",  alpha = 0.5))),
     "item-based CF pearson Z-score" = list(name="IBCF", param=(list(k = 7, method="pearson", normalize = "Z-score",  alpha = 0.5))),
-   	"item-based CF jaccard Z-score" = list(name="IBCF", param=(list(k = 5, method="jaccard", normalize = "Z-score",  alpha = 0.5))),
-	"item-based CF karypis Z-score" = list(name="IBCF", param=(list(k = 4, method="karypis", normalize = "Z-score",  alpha = 0.5))),
+   	"item-based CF jaccard Z-score" = list(name="IBCF", param=(list(k = 5, method="jaccard", normalize = "Z-score",  alpha = 0.5))),	
 	"item-based CF conditional Z-score" = list(name="IBCF", param=(list(k = 5, method="conditional", normalize = "Z-score",  alpha = 0.5)))
    )
 evalResults = evaluate(scheme,algorithms,n=c(1,3,5,10,20))
