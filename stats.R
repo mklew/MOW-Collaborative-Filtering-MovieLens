ad1067d132f91e080142d05678905b23791206a2
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-01-30 21:49:03 +0100

    Stats of MovieLense dataset

--- stats.R
+++ stats.R
@@ -1,24 +1,29 @@
 library('reshape2')
 library('ggplot2')
+library('recommenderlab')
+library('xtable')
+
+data(MovieLense)
 userRatingsFile = "u1.base"
 
 # getRatings reads all non missing ratings as vector
 
 
-data = createUsersMatrix(read.table(file.path("data",userRatingsFile)))
+#data = createUsersMatrix(read.table(file.path("data",userRatingsFile)))
+data=MovieLense
 plot1 = qplot(getRatings(data), binwidth = 1, 
   geom="histogram", xlab="Oceny", ylab="Freq",main="Histogram ocen", fill= ..count..)
 
 # przy zapisie nie ma polskich znaków ; (
-ggsave(plot=plot,file="histogram_ocen.pdf")
+ggsave(plot=plot1,file=file.path("doc", "img", "histogram_ocen.pdf") )
 
 plot2 = qplot(getRatings(normalize(data, method = "Z-score")),
   geom="histogram", xlab="Oceny", ylab="Freq",main="Histogram ocen znormalizowanych", fill=..count..)
-ggsave(plot2,file="histogram_ocen_norm.pdf")
+ggsave(plot2,file=file.path("doc", "img",  "histogram_ocen_norm.pdf") )
 
 
 # basic stats
-summary(getRatings(data))
+s <- summary(getRatings(data))
 
 minimalNumberOfRatings <- min(rowCounts(data)) ## Najmniejsza liczba ocen u jakiegoś usera
 print(paste("Najmniejsza liczba ocen", minimalNumberOfRatings))
@@ -34,5 +39,22 @@ print(paste("Największa średnia ocena", maximalAverageRating))
 
 averageNumberOfRatedMovies <- mean(rowCounts(data))
 print(paste("Średnia liczba filmów oceniona przez użytkownika", averageNumberOfRatedMovies))
+
+dt <- data.frame()
+dt <- rbind(dt, c(s[3]))
+dt <- rbind(dt, c(s[4]))
+dt <- rbind(dt, c(minimalNumberOfRatings))
+dt <- rbind(dt, c(maximalNumberOfRatings))
+dt <- rbind(dt, c(minimalAverageRating))
+dt <- rbind(dt, c(maximalAverageRating))
+dt <- rbind(dt, c(averageNumberOfRatedMovies))
+names(dt) <- c("wartość")
+
+row.names(dt) <- c("Mediana ocen", "Średnia ocena", "Najmniejsza liczba ocen", "Największa liczba ocen", "Najmniejsza średnia ocena", "Największa średnia ocena", "Średnia liczba filmów oceniona przez użytkownika")
+
+dtTable <- xtable(dt, caption="statystyki zbioru danych")
+	dtTableFile <- file(file.path("doc", "statystyki-zbioru.tex"))
+	writeLines(print(dtTable, type="latex"), dtTableFile)
+	close(dtTableFile)
 
 
